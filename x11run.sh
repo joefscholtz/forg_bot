9fdc9b352ae5bea08f4db66223f96d6e9de20eb6
Joe Ferreira Scholtz <joefscholtz@gmail.com>
2024-05-13 21:41:43 -0300

    add --rm flah to docker run so the container is automaticaly removed when stoped

--- x11run.sh
+++ x11run.sh
@@ -2,9 +2,11 @@
 
 SCRIPT_PATH=$(dirname "$0")
 DOCKER_USER="ros"
+
 docker run -it \
 	--user $DOCKER_USER --network=host --ipc=host -v $SCRIPT_PATH/../..:/home/$DOCKER_USER/ros2_ws \
 	-v $HOME/.gitconfig:/home/$DOCKER_USER/.gitconfig \
 	-e DISPLAY=$DISPLAY \
 	--name jumpy_frog \
+	--rm \
 	forg_bot:dev
