f4947d843b157a010b51cf11f15d22ecccb14d46
Joe Ferreira Scholtz <joefscholtz@gmail.com>
2024-07-09 20:20:30 -0300

    add container name to prompt

--- x11run.sh
+++ x11run.sh
@@ -3,10 +3,18 @@
 SCRIPT_PATH=$(dirname "$0")
 DOCKER_USER="ros"
 
+CONTAINER_NAME="forg_bot"
+
+temp=$(mktemp)
+echo "name=\"$CONTAINER_NAME\"" >>"$temp"
+
 docker run -it \
 	--user $DOCKER_USER --network=host --ipc=host -v $SCRIPT_PATH/../..:/home/$DOCKER_USER/ros2_ws \
 	-v $HOME/.gitconfig:/home/$DOCKER_USER/.gitconfig \
 	-e DISPLAY=$DISPLAY \
 	--name jumpy_frog \
 	--rm \
+	-v $temp:/run/.containerenv \
 	forg_bot:dev
+
+rm $temp
